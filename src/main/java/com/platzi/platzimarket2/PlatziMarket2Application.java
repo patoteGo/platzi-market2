d71e9b7621786e2408587b2a2d1f8085076ac356
Patricio Gonzalez <patote.gonzalez@gmail.com>
2020-10-10 21:04:37 -0300

    init

---
+++ src/main/java/com/platzi/platzimarket2/PlatziMarket2Application.java
@@ -0,0 +1,13 @@
+package com.platzi.platzimarket2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlatziMarket2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PlatziMarket2Application.class, args);
+    }
+
+}
